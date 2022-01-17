025ba5989210d5324b2d0ae408b4ddbae9130deb
mhrhabib <mhrhabib39@gmail.com>
2022-01-17 11:22:40 +0600

    Update login_screen.dart

--- lib/form_validation/login_screen.dart
+++ lib/form_validation/login_screen.dart
@@ -10,6 +10,11 @@ class LogInScreen extends StatefulWidget {
 
 class _LogInScreenState extends State<LogInScreen> {
   final formKey = GlobalKey<FormState>();
+
+  String email = '';
+  String password = '';
+
+  bool isVisible = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,28 +33,48 @@ class _LogInScreenState extends State<LogInScreen> {
                   //errorText: 'please enter  valid email',
                   border: OutlineInputBorder(),
                 ),
-                validator: (value) {
-                  if (!value!.contains("@")) {
+                validator: (String? value) {
+                  if (!value!.contains("@gmail.com")) {
                     return "please enter a valid email";
                   }
+                },
+                onSaved: (newValue) {
+                  email = newValue!;
                 },
               ),
               const SizedBox(height: 12),
               TextFormField(
-                decoration: const InputDecoration(
+                obscureText: isVisible ? true : false,
+                decoration: InputDecoration(
                   hintText: "password",
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        isVisible = !isVisible;
+                      });
+                    },
+                    icon: isVisible
+                        ? Icon(Icons.visibility_off)
+                        : Icon(Icons.visibility),
+                  ),
                   border: OutlineInputBorder(),
                 ),
                 validator: (value) {
                   if (value!.length < 6) {
-                    return "password at least 6 characters";
+                    return 'Password must be more than 5 characters';
                   }
+                },
+                onSaved: (newValue) {
+                  password = newValue!;
                 },
               ),
               const SizedBox(height: 12),
               ElevatedButton(
                 onPressed: () {
-                  if (formKey.currentState!.validate()) {}
+                  if (formKey.currentState!.validate()) {
+                    formKey.currentState!.save();
+                    print("your $email and $password");
+                  }
                 },
                 child: const Text('Submit'),
               ),
